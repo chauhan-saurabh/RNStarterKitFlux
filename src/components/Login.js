030f92e157dbf6f8b0b0bc2c134d1b48b3ff4857
Ravi kumar <ravi.kumar@smartdatainc.net>
2018-11-14 17:49:29 +0530

    Minor UI changes
    Minor UI changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,66 +29,118 @@ class Login extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{ margin: 10 }}>Email :</Text>
-        <TextInput
-          style={{
-            margin: 10,
-            borderBottomColor: "#dadada",
-            borderBottomWidth: 1
-          }}
-          value={this.state.email}
-          onChangeText={email => {
-            this.setState({ email });
-          }}
-          placeholder="email"
-        />
-        <Text style={{ margin: 10 }}>Password :</Text>
-        <TextInput
-          secureTextEntry={true}
-          style={{
-            margin: 10,
-            borderBottomColor: "#dadada",
-            borderBottomWidth: 1
-          }}
-          value={this.state.password}
-          onChangeText={password => {
-            this.setState({ password });
-          }}
-          placeholder="password"
-        />
+        <View style={{ flex: 0.35 }} />
+        <View style={{ flex: 0.05 }}>
+          <Text
+            style={{
+              marginLeft: 10,
+              marginTop: 8,
+              fontSize: width * 0.045,
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Email :
+          </Text>
+        </View>
+
         <View
           style={{
-            flex: 0.1,
-            backgroundColor: "#0080ff",
-            width: width * 0.5,
-            justifyContent: "center",
-            alignSelf: "center",
+            flex: 0.05,
+            backgroundColor: "white",
+            marginHorizontal: width * 0.03,
             borderRadius: width * 0.02
           }}
         >
-          {this.props.loginReducer.loginLoader ? (
-            <TouchableOpacity style={{ margin: 10, alignItems: "center" }}>
-              <ActivityIndicator />
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity
-              style={{ margin: 10, alignItems: "center" }}
-              onPress={() => {
-                this.handleSubmit();
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: width * 0.04,
-                  textAlign: "center",
-                  fontWeight: "bold"
+          <TextInput
+            style={{
+              margin: 10,
+              borderBottomColor: "#dadada",
+              borderBottomWidth: 1,
+              fontSize: width * 0.04
+            }}
+            value={this.state.email}
+            onChangeText={email => {
+              this.setState({ email });
+            }}
+            placeholder="email"
+          />
+        </View>
+        <View style={{ flex: 0.05 }}>
+          <Text
+            style={{
+              marginLeft: 10,
+              marginTop: 8,
+              fontSize: width * 0.045,
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Password :
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 0.05,
+            backgroundColor: "white",
+            marginHorizontal: width * 0.03,
+            borderRadius: width * 0.02
+          }}
+        >
+          <TextInput
+            secureTextEntry={true}
+            style={{
+              margin: 10,
+              borderBottomColor: "#dadada",
+              borderBottomWidth: 1,
+              fontSize: width * 0.04
+            }}
+            value={this.state.password}
+            onChangeText={password => {
+              this.setState({ password });
+            }}
+            placeholder="password"
+          />
+        </View>
+        <View style={{ flex: 0.1 }}>
+          <View style={{ flex: 0.15 }} />
+          <View
+            style={{
+              flex: 0.75,
+              backgroundColor: "#0080ff",
+              width: width * 0.5,
+              justifyContent: "center",
+              alignSelf: "center",
+              borderRadius: width * 0.02
+            }}
+          >
+            {this.props.loginReducer.loginLoader ? (
+              <TouchableOpacity style={{ margin: 10, alignItems: "center" }}>
+                <ActivityIndicator />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                style={{ margin: 10, alignItems: "center" }}
+                onPress={() => {
+                  this.handleSubmit();
                 }}
               >
-                Login
-              </Text>
-            </TouchableOpacity>
-          )}
+                <Text
+                  style={{
+                    fontSize: width * 0.045,
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    color: "white"
+                  }}
+                >
+                  Login
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
         </View>
+
+        <View style={{ flex: 0.35 }} />
       </View>
     );
   }
