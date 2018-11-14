c3455cf79b344ebf84145204401229cb2b43d516
Ravi kumar <ravi.kumar@smartdatainc.net>
2018-11-14 17:31:54 +0530

    Minor UI changes
    Minor UI changes

--- src/components/ScrollableTab.js
+++ src/components/ScrollableTab.js
@@ -10,12 +10,12 @@ export default () => {
   return (
     <ScrollableTabView
       style={{ backgroundColor: "white" }}
-      tabBarBackgroundColor="#333435"
+      tabBarBackgroundColor="#0080ff"
       tabBarActiveTextColor="#fff"
       tabBarInactiveTextColor="#BDC3C7"
       tabBarTextStyle={{ fontSize: 20 }}
       tabBarPosition={"bottom"}
-      tabBarUnderlineStyle={{ backgroundColor: "#3FC380" }}
+      tabBarUnderlineStyle={{ backgroundColor: "white" }}
       renderTabBar={() => <ScrollableTabBar />}
     >
       <Tab1 tabLabel="Tab 1" label="Page #1" />
